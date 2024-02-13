c0bd3825d999c17a7420161a80dec76a73914c02
Sean Connolly <sean.connolly@northwestern.edu>
2024-02-13 18:14:46 +0000

    companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,11 @@
+class CompaniesController < ApplicationController
+
+def index
+  render :template => "companies/index"
+  
+
+
+end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/tacos", :controller =>"tacos", :action => "index"
   get"/dice", :controller =>"dice", :action => "index"
+  resources "companies"
+  # resources gives us get"/companies", :controller =>"companies", "action" => "index"
 end
